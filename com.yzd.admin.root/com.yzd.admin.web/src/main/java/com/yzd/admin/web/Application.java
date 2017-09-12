232049ebee340b46a857d9c81ebaf373b5b6cf96
yzd <tedijoteamo@163.com>
2017-09-12 13:22:15 +0800

    server.properties与application.properties共存，application存储共有配置，server存储端口号等单独配置

--- com.yzd.admin.root/com.yzd.admin.web/src/main/java/com/yzd/admin/web/Application.java
+++ com.yzd.admin.root/com.yzd.admin.web/src/main/java/com/yzd/admin/web/Application.java
@@ -2,11 +2,15 @@ package com.yzd.admin.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by zd.yao on 2017/6/9.
  */
 @SpringBootApplication
+//如果发现missing.properties不存在，则抛出异常，也可以使用ignoreResourceNotFound=true去忽略
+//@PropertySource(value = "classpath:server.properties",ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:server.properties")
 public class Application {
 
     public static void main(String[] args) {
